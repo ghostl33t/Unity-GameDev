618b94ab804d43b0cfc238e19d8c19a27c543e95
ghostl33t <alibegovicjasim1337@gmail.com>
2020-12-05 15:42:36 +0100

    SquareMove V1.0.1 Omoguceno kretanje kockice i pracenje main kamerom

---
+++ SquareMove/Assets/Script_ObjPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_ObjPlayer : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerMovement();
+    }
+
+    public void playerMovement()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("w"))
+         {
+             rb.AddForce(0, 0, 500 * Time.deltaTime);
+         }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        }
+    }
+}
